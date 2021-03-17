0b1d271b5431e244810ae3871c1175eb39864525
dkssyddico <dkssyddico@naver.com>
2021-03-17 23:53:24 +0900

    martaferri: JS finished(change background color)

--- src/js/main.js
+++ src/js/main.js
@@ -27,8 +27,31 @@ function debounce(func, wait = 20, immediate = true) {
   };
 }
 
+const body = document.querySelector('body');
+
+//158, 875,1592, 2309, 3026, 3743, 4460
+
+const changeColor = () => {
+  let windowScrollY = window.scrollY;
+  if (windowScrollY >= 158) {
+    body.classList.remove('basicBg');
+    body.classList.add('plum');
+  }
+  if (windowScrollY >= 1592) {
+    body.classList.remove('plum');
+    body.classList.add('powderBlue');
+  }
+
+  if (windowScrollY >= 3026) {
+    body.classList.remove('powderBlue');
+    body.classList.add('rosyBrown');
+  }
+};
+
 function checkSlide() {
   sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    changeColor();
     const sectionHeight = section.getBoundingClientRect().height;
     const slideInAt = window.scrollY + window.innerHeight - sectionHeight / 2;
     const imageBottom = section.offsetTop + sectionHeight;
