341788ac11a6f12fb3d2f88968fd1e0f9c855fc2
dkssyddico <dkssyddico@naver.com>
2021-04-25 15:33:01 +0900

    kinseygross: CSS, JS complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const mainCards = document.querySelectorAll('.main-card');
+
+const HIDDEN_CN = 'hidden';
+
+mainCards.forEach((mainCard) => {
+  mainCard.addEventListener('mouseover', () => {
+    const subCard = mainCard.querySelector('.sub-container');
+    subCard.classList.remove('hidden');
+  });
+  mainCard.addEventListener('mouseleave', () => {
+    const subCard = mainCard.querySelector('.sub-container');
+    subCard.classList.add('hidden');
+  });
+});
